485adcfbac2750fd127489d7a4acf0f23a507096
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-03-22 19:58:11 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+
+window.customerName = 'bob';
+
+function upperCaseCustomerName(){
+    window.customerName = window.customerName(toUpperCase);
+}
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer){
+    window.bestCustomer = newBestCustomer;
+
+}
+
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob';
+}
+
+
+function changeLeastFavoriteCustomer(){
+    const changeLeastFavoriteCustomer = '!bob';
+}
+
+ 
